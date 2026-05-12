4dc2e25850079985bb1ae54794a49635da47e2a2
all340 <all6765666@gmail.com>
2026-05-13 02:10:39 +0300

    Update and rename README.md to music_bot.py

---
+++ music_bot.py
@@ -0,0 +1,66 @@
+import os
+import yt_dlp
+
+from telegram import Update
+from telegram.ext import (
+    Application,
+    MessageHandler,
+    filters,
+    ContextTypes,
+)
+
+TOKEN = os.getenv("TOKEN")
+
+
+async def music(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.message.text
+
+    await update.message.reply_text("🔍 Ищу музыку...")
+
+    ydl_opts = {
+        'format': 'bestaudio',
+        'noplaylist': True,
+        'default_search': 'ytsearch1',
+        'outtmpl': 'music.%(ext)s',
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.extract_info(query, download=True)
+
+        file_name = None
+
+        for file in os.listdir():
+            if file.startswith("music."):
+                file_name = file
+                break
+
+        if not file_name:
+            await update.message.reply_text("Файл не найден")
+            return
+
+        with open(file_name, "rb") as audio:
+            await update.message.reply_audio(
+                audio=audio,
+                title=query
+            )
+
+        os.remove(file_name)
+
+    except Exception as e:
+        await update.message.reply_text(f"Ошибка: {e}")
+
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(
+        MessageHandler(filters.TEXT, music)
+    )
+
+    print("Бот запущен")
+
+    app.run_polling()
+
+
+main()
