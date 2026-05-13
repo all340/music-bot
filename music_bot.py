ba0eac2732f911538ec7e5f6e2326a3802719a12
all340 <all6765666@gmail.com>
2026-05-13 04:59:19 +0300

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -1,11 +1,13 @@
 import os
+import yt_dlp
+
 from telegram import Update
 from telegram.ext import (
     Application,
-    CommandHandler,
     MessageHandler,
-    ContextTypes,
     filters,
+    ContextTypes,
+    CommandHandler,
 )
 
 TOKEN = os.getenv("TOKEN")
@@ -13,21 +15,50 @@ TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "🎵 Привет!\n"
-        "Напиши название песни.\n\n"
-        "Например:\n"
-        "Alan Walker Faded"
+        "🎵 Отправь название музыки"
     )
 
 
 async def music(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.message.text
 
-    await update.message.reply_text(
-        f"🔎 Ты ищешь: {query}\n\n"
-        "❗ Сейчас Railway блокирует YouTube/SoundCloud.\n"
-        "Для полноценного музыкального бота нужен VPS или cookies."
-    )
+    await update.message.reply_text("🔎 Ищу музыку...")
+
+    ydl_opts = {
+        'format': 'bestaudio',
+        'noplaylist': True,
+        'default_search': 'ytsearch1',
+        'outtmpl': 'music.%(ext)s',
+        'cookiefile': 'cookies.txt',
+        'quiet': True,
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(query, download=True)
+
+        file_name = None
+
+        for file in os.listdir():
+            if file.startswith("music."):
+                file_name = file
+                break
+
+        if not file_name:
+            await update.message.reply_text("❌ Музыка не найдена")
+            return
+
+        title = info.get("title", "music")
+
+        await update.message.reply_audio(
+            audio=open(file_name, 'rb'),
+            title=title,
+        )
+
+        os.remove(file_name)
+
+    except Exception as e:
+        await update.message.reply_text(f"❌ Ошибка:\n{e}")
 
 
 def main():
@@ -38,7 +69,7 @@ def main():
         MessageHandler(filters.TEXT & ~filters.COMMAND, music)
     )
 
-    print("Bot started")
+    print("Бот запущен!")
 
     app.run_polling()
 
